24a9af7eb30c3cdd03b2d16a72f5f8bbf5f88dd4
Zander <28664144+ZandercraftGames@users.noreply.github.com>
2021-03-01 23:54:14 -0500

    Done for the night. More work to come in the morning.

---
+++ src/main/java/ca/zandercraft/doorknockerforge/KnockSounds.java
@@ -0,0 +1,12 @@
+package ca.zandercraft.doorknockerforge;
+
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvent;
+
+public class KnockSounds {
+    public static final ResourceLocation DOOR_KNOCK = new ResourceLocation("doorknockermod:door_knock");
+    public static SoundEvent DOOR_KNOCK_EVENT = new SoundEvent(DOOR_KNOCK);
+    public static final ResourceLocation TRAPDOOR_KNOCK = new ResourceLocation("doorknockermod:trapdoor_knock");
+    public static SoundEvent TRAPDOOR_KNOCK_EVENT = new SoundEvent(TRAPDOOR_KNOCK);
+}
